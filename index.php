677566bcb74916c8ad144c91f3cdfc9e3c4ba7bc
leonardo15043 <lahernandez15043@gmail.com>
2018-12-21 12:51:01 -0500

    Metadatos del Plugin y menu del administrador

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Mi primer plugin
+Description: Crea un menú de configuración de un texto para mostrar en el pie del cms
+Author: leonardo15043
+Author URI: https://www.example.com/
+*/
+
+add_action("admin_menu", "menu_admin_mi_modulo");
+function menu_admin_mi_modulo() {
+  add_menu_page('Configuración Modulo', 'Configuración Modulo', 'manage_options', 'menu_config_mimodulo', 'controller_modulo');
+}
